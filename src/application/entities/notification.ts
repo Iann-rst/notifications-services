cbb7b6d8aa68fbe4d949f68562dcc56f1bad908a
Iann-rst <iannrst11@gmail.com>
2022-12-13 20:56:17 -0300

    feat: Entidade de notificação

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,62 @@
+import { Replace } from './../../helpers/Replace';
+import { Content } from './content';
+/**
+ * Entidade de notificações - utilizando getters e setters
+ */
+
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  // Constructor
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  //Getters e Setters
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
