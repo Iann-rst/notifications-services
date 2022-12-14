208160d65f5ca5b03bd4f277b8425e0de8fec2fc
Iann-rst <iannrst11@gmail.com>
2022-12-13 21:50:34 -0300

    feat: Caso de Uso para enviar uma notificação

---
+++ src/application/use-cases/send-notification.ts
@@ -0,0 +1,37 @@
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, content, category } = request;
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    //Quando criar a notificação, quero persistir essa notificação no banco de dados
+
+    await this.notificationsRepository.create(notification);
+
+    return {
+      notification,
+    };
+  }
+}
