493e552d044db3383ebc119f8d034bfc3b26f8a0
Iann-rst <iannrst11@gmail.com>
2022-12-14 21:22:21 -0300

    fix: Criação da notificação com o makeNotification

--- src/application/use-cases/count-recipient-notifications.spec.ts
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -1,5 +1,4 @@
-import { Content } from '@application/entities/content';
-import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CountRecipientNotifications } from './count-recipient-notifications';
 
@@ -11,25 +10,19 @@ describe('Count recipients Notifications', () => {
     );
 
     await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Conteúdo de teste'),
+      makeNotification({
         recipientId: 'recipient-1',
       }),
     );
 
     await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Conteúdo de teste'),
+      makeNotification({
         recipientId: 'recipient-1',
       }),
     );
 
     await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Conteúdo de teste'),
+      makeNotification({
         recipientId: 'recipient-2',
       }),
     );
