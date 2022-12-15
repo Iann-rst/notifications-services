75916c5b9cde9bc46dbaa0f5c00fbc8998546d26
Iann-rst <iannrst11@gmail.com>
2022-12-14 22:08:22 -0300

    feat: Entidades existentes (ID)

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -19,8 +19,11 @@ export class Notification {
   private _id: string;
 
   // Constructor
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
